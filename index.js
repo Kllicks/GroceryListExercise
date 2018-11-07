7727e9173f975d7ef6b327f5fb2b51c7751775d8
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-07 18:30:48 -0500

    adds testing to all the CRUD methods within the Groceries class

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 require('dotenv').config();
 
 const Groceries = require('./models/Groceries');
+
+// FOR TESTING:
+// ============
+
+// CREATE
+// ------
+// Groceries.add(`goldfish`, 5)
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// RETRIEVE
+// --------
+Groceries.getAll()
+    .then(results => {
+        console.log(results);
+        console.log('those were the groceries');
+    })
+
+// Groceries.getById(5)
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// UPDATE
+// ------
+
+
+// DELETE
+// ------
+// Groceries.deleteById(10)
+//     .then(result => {
+//         console.log(result.rowCount);
+//     })
--- models/Groceries.js
+++ models/Groceries.js
@@ -6,4 +6,58 @@ class Groceries {
         this.name = name;
         this.quantity = quantity;
     }
+
+    // CREATE
+    static add(name, quantity) {
+        return db.one(`insert into groceries (name, quantity)
+            values
+                ($1, $2)
+            returning id
+        `, [name, quantity]) 
+    }
+
+    // RETRIEVE
+    static getAll() {
+        return db.any('select * from groceries').then(groceryArray => {
+            let instanceArray = groceryArray.map(groceryObj => {
+                let u = new Groceries(groceryObj.id, groceryObj.name, groceryObj.quantity);
+                return u;
+            });
+            return instanceArray;
+        })
+    }
+
+    static getById(id){
+        return db.one(`select * from groceries where id = $1`, [id])
+            .then(result => {
+                const u = new Groceries(result.id, result.name, result.quantity)
+                return u;
+            })
+    }
+
+    // UPDATE
+    updateQuantity(quantity) {
+        return db.result(`update groceries
+                            set quantity=$2
+                            where id=$1`, [this.id, quantity]);
+    }
+    
+    updateName(name) {
+        return db.result(`update groceries
+                            set name=$2
+                            where id=$1`, [this.id, name]);
+    }
+
+
+    // DELETE
+    delete() {
+        return db.result(`delete from groceries where id = $1`, [this.id]);
+    }
+    
+    static deleteById(id){
+        return db.result(`delete from groceries where id = $1`, [id]);
+    }
 }
+
+// Export
+module.exports = Groceries;
